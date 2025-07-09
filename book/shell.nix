e35a8064799d9dad430817384bb3214cb5237197
Nick Spinale <nick@nickspinale.com>
2025-07-09 00:30:41 -0700

    Update Nixpkgs

--- book/shell.nix
+++ book/shell.nix
@@ -8,11 +8,11 @@ let
 
   nixpkgsPath =
     let
-      rev = "3a458f7c763ca62c6bf454b8d828bd86b7250671";
+      rev = "368c3a52976f7848bebecd1d79bcdbf212bae189"; # branch release-25.05
     in
       builtins.fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-        sha256 = "sha256:119wjr45ypvxhz7qxhydcrllrvlyqxla0ymb0l0wgaqrkdhff9xb";
+        sha256 = "sha256:12cyfh0w84lqwvcy5zkk6r2x22anjhkylg875w4m5y0difk7sc2d";
       };
 
   pkgs = import nixpkgsPath {};
